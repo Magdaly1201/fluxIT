5c22cc98dd6a490265a15347da9eb5c4a062888d
Magdaly Santos <magdaly1201@gmail.com>
2020-11-15 20:11:46 -0300

    agregar excepciones

--- src/main/java/ar/com/fluxit/candidates/controller/RestExceptionAdvice.java
+++ src/main/java/ar/com/fluxit/candidates/controller/RestExceptionAdvice.java
@@ -1,8 +1,8 @@
 package ar.com.fluxit.candidates.controller;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,45 +19,42 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.client.HttpClientErrorException.BadRequest;
 
+import ar.com.fluxit.candidates.exception.CandidateNotFoundException;
+import ar.com.fluxit.candidates.model.dto.CandidateResponseDTO;
+
 
 @ControllerAdvice
 public class RestExceptionAdvice {
 	private final Logger logger = LoggerFactory.getLogger(this.getClass());
 	
 	
-//	
-//	@ExceptionHandler(CarNotFoundException.class)
-//	@ResponseBody
-//	@ResponseStatus(HttpStatus.NOT_FOUND)
-//	public ResponseEntity<?> carResponseExceptionHanlder(CarNotFoundException ex) {
-//		String className = ex.getStackTrace()[0].getClassName();
-//		int line = ex.getStackTrace()[0].getLineNumber();
-//		logger.error("Exception: "+ex.getMessage()+ " - Class: "+className +" - line: "+ line);
-//		this.responseBusiness.loadError(404, ex.getMessage(), ex.getMessage());
-//		return this.responseBusiness.getOutput();
-//	}
-//
-//	//BindException
-//	@ResponseStatus(HttpStatus.BAD_REQUEST)
-//	@ResponseBody
-//	@ExceptionHandler(BindException.class)
-//	public Response handleBindException(BindException ex) {
-//		
-//		List<String> errors = new ArrayList<String>();
-//	    for (FieldError error : ex.getBindingResult().getFieldErrors()) {
-//	        errors.add(error.getField() + ": " + error.getDefaultMessage());
-//	        
-//	        this.responseBusiness.loadError(Integer.parseInt(error.getCode()), error.getField() +" -"+ error.getDefaultMessage() , error.getField() +" -"+ error.getDefaultMessage());
-//	    }
-//	    for (ObjectError error : ex.getBindingResult().getGlobalErrors()) {
-//	        errors.add(error.getObjectName() + ": " + error.getDefaultMessage());
-//	        
-//	        System.out.println("error.getObjectName() "+error.getObjectName());
-//	        System.out.println("error.getDefaultMessage() "+error.getDefaultMessage());
-//	        this.responseBusiness.loadError(/*error.getCode()*/1, error.getObjectName() +" -"+ error.getDefaultMessage() , error.getObjectName() +" -"+ error.getDefaultMessage());
-//	    }    
-//	    return this.responseBusiness.getOutput();
-//	}
-//	
+	
+	@ExceptionHandler(CandidateNotFoundException.class)
+	@ResponseBody
+	public ResponseEntity<Map<String, Object>> carResponseExceptionHanlder(CandidateNotFoundException ex) {
+		Map<String, Object> response = new HashMap<>();
+		String className = ex.getStackTrace()[0].getClassName();
+		int line = ex.getStackTrace()[0].getLineNumber();
+		logger.error("Exception: "+ex.getMessage()+ " - Class: "+className +" - line: "+ line);
+		response.put("errors", ex.getMessage());
+		return new ResponseEntity<Map<String, Object>>(response, HttpStatus.BAD_REQUEST);
+
+	}
+
+	@ResponseBody
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<Map<String, Object>> handleValidation(MethodArgumentNotValidException ex){
+		Map<String, Object> response = new HashMap<>();
+		if(ex.getBindingResult().hasErrors()) {
+		List<String> errors = ex.getBindingResult().getFieldErrors()
+				.stream()
+				.map(err ->{
+					return "El campo  : '"+ err.getField() +"' "+err.getDefaultMessage();
+				})
+				.collect(Collectors.toList());
+		response.put("errors", errors);
+		}
+		return new ResponseEntity<Map<String, Object>>(response, HttpStatus.BAD_REQUEST);
+	}
 	
 }
