4ddbec849c91e323a0b8532c16d64ddb0a2b1825
Magdaly Santos <magdaly1201@gmail.com>
2020-11-15 20:27:57 -0300

    agregar import

--- src/main/java/ar/com/fluxit/candidates/controller/RestExceptionAdvice.java
+++ src/main/java/ar/com/fluxit/candidates/controller/RestExceptionAdvice.java
@@ -3,24 +3,18 @@ package ar.com.fluxit.candidates.controller;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.http.converter.HttpMessageNotReadableException;
-import org.springframework.validation.BindException;
-import org.springframework.validation.FieldError;
-import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.client.HttpClientErrorException.BadRequest;
-
 import ar.com.fluxit.candidates.exception.CandidateNotFoundException;
-import ar.com.fluxit.candidates.model.dto.CandidateResponseDTO;
 
 
 @ControllerAdvice
