cb7b521841b5ff470dc2ba25362d7114ecc563d4
Magdaly Santos <magdaly1201@gmail.com>
2020-11-15 20:12:13 -0300

    agregar metodo para buscar por documentNumber y Fullname

--- src/main/java/ar/com/fluxit/candidates/repository/ICandidateRepository.java
+++ src/main/java/ar/com/fluxit/candidates/repository/ICandidateRepository.java
@@ -2,7 +2,11 @@
  * 
  */
 package ar.com.fluxit.candidates.repository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
 import ar.com.fluxit.candidates.entity.Candidate;
 
 
@@ -14,5 +18,7 @@ import ar.com.fluxit.candidates.entity.Candidate;
  */
 public interface ICandidateRepository extends JpaRepository<Candidate,Long>{ 
 
+	@Query()
+	public Page<Candidate> findAllByDocumentNumberAndFullName(int documentNumber,int fullName,Pageable pageable);
 }
 
