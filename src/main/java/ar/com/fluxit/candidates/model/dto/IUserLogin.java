386da7cc5fe32ea637db9f5cf27833335fa2cc38
Magdaly <magdaly.santos@equifax.com>
2020-11-15 22:58:50 -0300

    creacion de dto user para login , Iuser con todos los valores

---
+++ src/main/java/ar/com/fluxit/candidates/model/dto/IUserLogin.java
@@ -0,0 +1,17 @@
+package ar.com.fluxit.candidates.model.dto;
+
+public interface IUserLogin {
+
+	/**
+	 * @return the username
+	 */
+	String getUsername();
+
+	/**
+	 * @return the password
+	 */
+	String getPassword();
+
+
+}
+
---
+++ src/main/java/ar/com/fluxit/candidates/model/jwt/UserLoginDTO.java
@@ -0,0 +1,46 @@
+package ar.com.fluxit.candidates.model.jwt;
+
+import javax.validation.constraints.NotEmpty;
+
+import ar.com.fluxit.candidates.model.dto.IUserLogin;
+
+
+public class UserLoginDTO implements IUserLogin{
+	
+	@NotEmpty(message = "no puede ser vacio.")
+	private String username;
+	
+	@NotEmpty(message = "no puede ser vacio.")
+	private String password;
+
+	/**
+	 * @return the username
+	 */
+	public String getUsername() {
+		return username;
+	}
+
+	/**
+	 * @return the password
+	 */
+	public String getPassword() {
+		return password;
+	}
+
+	/**
+	 * @param username the username to set
+	 */
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	/**
+	 * @param password the password to set
+	 */
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
+
+}
