c10667bfa6e8efbb5437e8b24939484689088b22
Magdaly Santos <magdaly1201@gmail.com>
2020-11-16 10:34:14 -0300

    llamada al metodo generator

--- src/main/java/ar/com/fluxit/candidates/controller/AuthenticationController.java
+++ src/main/java/ar/com/fluxit/candidates/controller/AuthenticationController.java
@@ -6,7 +6,11 @@ package ar.com.fluxit.candidates.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
+import ar.com.fluxit.candidates.exception.AuthenticationFailedException;
+import ar.com.fluxit.candidates.model.jwt.UserJWT;
 import ar.com.fluxit.candidates.model.jwt.UserLoginDTO;
+import ar.com.fluxit.candidates.security.GeneratorJWT;
+import ar.com.fluxit.candidates.service.IUserService;
 
 /**
  * @author Magdaly Santos
@@ -15,8 +19,15 @@ import ar.com.fluxit.candidates.model.jwt.UserLoginDTO;
 @Controller
 public class AuthenticationController {
 
-	public String generator(UserLoginDTO login) {
-		return null;
+	@Autowired
+	private GeneratorJWT generator;
+	
+	@Autowired
+	private IUserService userService;
+	
+	public String generator(UserLoginDTO login) throws AuthenticationFailedException {
+		UserJWT userJWT = this.userService.existUser(login);
+		return this.generator.generate(userJWT);
 	}
 	
 }
