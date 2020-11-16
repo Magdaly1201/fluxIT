8ef4c41d06570db93ae8a367d2a9e02d0f07e2f0
Magdaly <magdaly.santos@equifax.com>
2020-11-15 23:08:53 -0300

    creacion de clase generator JWT

---
+++ src/main/java/ar/com/fluxit/candidates/security/GeneratorJWT.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+package ar.com.fluxit.candidates.security;
+
+import org.springframework.stereotype.Component;
+
+import ar.com.fluxit.candidates.model.jwt.UserJWT;
+import constant.JwtConstants;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+
+
+/**
+ * @author mxs690
+ *
+ */
+@Component
+public class GeneratorJWT {
+
+	public String generate(UserJWT userJWT) {
+		Claims claims = Jwts.claims().setSubject(userJWT.getUsername());
+		claims.put(JwtConstants.USER_ID, String.valueOf(userJWT.getId()));
+		claims.put(JwtConstants.ROLE, userJWT.getRole());
+		return Jwts.builder().setClaims(claims).signWith(SignatureAlgorithm.HS256, JwtConstants.YOUR_SECRET).compact();
+
+	}
+}
