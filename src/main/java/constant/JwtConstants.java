16dbc9cf5af4a7a8bf0b5dac0dcdc5e137fb5dbc
Magdaly <magdaly.santos@equifax.com>
2020-11-15 23:11:03 -0300

    creacion de JWT Constants

---
+++ src/main/java/constant/JwtConstants.java
@@ -0,0 +1,15 @@
+package constant;
+
+public class JwtConstants {
+	
+	public final static String YOUR_SECRET = "secret_fluxIT";
+
+	public final static String USER_ID = "userId";
+
+	public final static String ROLE = "role";
+
+	public final static String AUTHORIZATION_HEADER = "Authorization";
+
+	public final static String BEARER_TOKEN = "Bearer ";
+	
+}
