8f6ffc01408fc56911bee89b49c05f508263b7fb
Magdaly Santos <magdaly1201@gmail.com>
2020-11-16 17:36:24 -0300

    modificacion de mensage

--- src/main/java/ar/com/fluxit/candidates/exception/AuthenticationFailedException.java
+++ src/main/java/ar/com/fluxit/candidates/exception/AuthenticationFailedException.java
@@ -16,7 +16,7 @@ public class AuthenticationFailedException extends CustomException{
 	 * 
 	 */
 	private static final long serialVersionUID = -856728934136263657L;
-	private static final ExceptionCodes exceptionCode = ExceptionCodes.CANDIDATE_NOT_FOUND;	
+	private static final ExceptionCodes exceptionCode = ExceptionCodes.USERNAME_PASSWORD_NO_MATCH;	
 	
 	public AuthenticationFailedException() {
 		super(exceptionCode.getCode(), exceptionCode.getUserMessage(), exceptionCode.getSystemMessage());
