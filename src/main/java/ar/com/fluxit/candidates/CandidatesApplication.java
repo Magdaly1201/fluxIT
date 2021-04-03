c0123d98bb66b8543b11495a0a21ccb33497e273
MAGDA <MAGDA@MAGDY>
2021-04-03 01:44:24 -0300

    format

--- src/main/java/ar/com/fluxit/candidates/CandidatesApplication.java
+++ src/main/java/ar/com/fluxit/candidates/CandidatesApplication.java
@@ -8,13 +8,13 @@ import org.springframework.context.annotation.Bean;
 @SpringBootApplication
 public class CandidatesApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CandidatesApplication.class, args);
-	}
-	
-	@Bean
-	public ModelMapper modelMapper() {
-	    return new ModelMapper();
-	}
-	
+    public static void main(String[] args) {
+	SpringApplication.run(CandidatesApplication.class, args);
+    }
+
+    @Bean
+    public ModelMapper modelMapper() {
+	return new ModelMapper();
+    }
+
 }
--- src/main/java/ar/com/fluxit/candidates/repository/ICandidateRepository.java
+++ src/main/java/ar/com/fluxit/candidates/repository/ICandidateRepository.java
@@ -2,6 +2,7 @@
  * 
  */
 package ar.com.fluxit.candidates.repository;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -17,11 +18,9 @@ import ar.com.fluxit.candidates.entity.Candidate;
  * @author Magdaly Santos
  *
  */
-public interface ICandidateRepository extends JpaRepository<Candidate,Long>{ 
+public interface ICandidateRepository extends JpaRepository<Candidate, Long> {
 
-	@Query("select c From Candidate c where (c.documentNumber=?1 or ?1 is null) and ( c.fullName=?2 or ?2 is null)")
-	public Page<Candidate> findAllByDocumentFullname(Integer documentNumber, String fullName, Pageable pageable);
+    @Query("select c From Candidate c where (c.documentNumber=?1 or ?1 is null) and ( c.fullName=?2 or ?2 is null)")
+    public Page<Candidate> findAllByDocumentFullname(Integer documentNumber, String fullName, Pageable pageable);
 
-	
 }
-
